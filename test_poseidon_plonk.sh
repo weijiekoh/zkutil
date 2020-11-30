35bff75d97a4cae51988cc239e8ca25cfe38249d
Koh Wei Jie <contact@kohweijie.com>
2020-11-30 12:13:58 +0000

    locked circom and snarkjs versions; updated process_circom_circuit.sh to use latest snarkjs commands; commented out line that post-processes the witness as it is no longer needed

--- test_poseidon_plonk.sh
+++ test_poseidon_plonk.sh
@@ -7,7 +7,7 @@ CIRCUIT_DIR=$DIR"/testdata/poseidon"
 # from zksync/infrastructure/zk/src/run/run.ts
 echo "Step1: download universal setup file"
 pushd keys/setup
-axel -c https://universal-setup.ams3.digitaloceanspaces.com/setup_2^20.key || true
+axel -ac https://universal-setup.ams3.digitaloceanspaces.com/setup_2^20.key || true
 popd
 
 echo "Step2: compile circuit and calculate witness using snarkjs"
